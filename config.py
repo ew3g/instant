87a4c17c706b8ea35e0d90f68549bed11edbe3d3
edilson <edilson.w3g@gmail.com>
2019-04-26 16:23:32 -0300

    keys

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 CONFIG = {
-  'pypexels_api_key':'563492ad6f91700001000001ed29a20a19ff4fe28cc40b6feb4ccbee',
+  'pypexels_api_key':'yourkey',
   'instagram_ratio_photo':512,
   'instagram_login':'edilson.w3g@gmail.com',
   'instagram_password':'yourpassword'
